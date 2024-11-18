9fed0394a783180426ed4d947d949633a2807bbc
fnabiyevuz <fnabiyevuz@gmail.com>
2024-11-18 17:12:38 +0500

    first commit

---
+++ src/components/channel/channel.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material"
+import { Link } from "react-router-dom"
+
+const Channel = () => {
+
+  return (
+    <Link to={'/'}>
+        <Button>Home</Button>
+    </Link>
+  )
+}
+
+export default Channel
